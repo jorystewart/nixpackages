05800278e4ab3291c2a8af7d678f44c7b4ee2176
Jory Stewart <jorymstewart@gmail.com>
2026-04-23 09:06:02 +0900

    install desktop item

--- curseforge-updater/curseforge-updater.nix
+++ curseforge-updater/curseforge-updater.nix
@@ -1,13 +1,27 @@
 { pkgs ? import <nixpkgs> {}, appimageTools ? pkgs.appimageTools, fetchurl ? pkgs.fetchurl }:
 
 let
-  pname = "CurseForge_Updater";
-  version = "";
+  pname = "curseForge_updater";
+  version = "1.300.0.32036";
 
   src = fetchurl {
     url = "https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage";
     hash = "sha256-wMAZwtbJ9eMtyFgVYuoVUEKzfDPuLOAWYXfIAItZgg8=";
   };
 
+  appimageContents = appimageTools.extractType1 {
+    inherit pname version src;
+    postExtract = ''
+      substituteInPlace $out/curseforge.desktop --replace-fail 'Exec=AppRun' 'Exec=curseforge'
+    '';
+  };
+
 in 
-  appimageTools.wrapType2 { inherit pname version src; }
+  appimageTools.wrapType2 rec {
+    inherit pname version src;
+
+    meta = {
+      description = "World of Warcraft Addon Updater";
+      platforms = [ "x86_64-linux" ];
+    };
+  }
